78eccf5de9aa718fd788513e268e565f8dd268a1
Leogjestv <leonargj@stud.ntnu.no>
2025-10-07 10:08:44 +0200

    Test LED

--- src/blunke_sensor.ino
+++ src/blunke_sensor.ino
@@ -1,8 +1,15 @@
 #include <Arduino.h>
 void setup() {
     // put your setup code here, to run once:
+        pinMode(LED_BUILTIN, OUTPUT);
     }
 
 void loop() {
     // put your main code here, to run repeatedly:
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.println("PÅ");
+        delay(500);
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("AV");
+        delay(500);
     }
