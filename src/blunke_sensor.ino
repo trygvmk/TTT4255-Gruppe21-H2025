38d248b1d130a510ce7c9da18c9560145ab54705
Leogjestv <leonargj@stud.ntnu.no>
2025-10-07 10:16:14 +0200

    Test monitor

--- src/blunke_sensor.ino
+++ src/blunke_sensor.ino
@@ -1,15 +1,41 @@
-#include <Arduino.h>
+#include <Arduino_LSM6DS3.h>
+
 void setup() {
-    // put your setup code here, to run once:
-        pinMode(LED_BUILTIN, OUTPUT);
-    }
+  Serial.begin(9600);
+  while (!Serial);
+
+  // Start LSM6DS3 (akselerometer + gyroskop)
+  if (!IMU.begin()) {
+    Serial.println("Feil: Kunne ikke starte IMU!");
+    while (1);
+  }
+  Serial.println("IMU (akselerometer og gyroskop) aktivert.");
+}
 
 void loop() {
-    // put your main code here, to run repeatedly:
-        digitalWrite(LED_BUILTIN, HIGH);
-        Serial.println("PÅ");
-        delay(500);
-        digitalWrite(LED_BUILTIN, LOW);
-        Serial.println("AV");
-        delay(500);
-    }
+  float ax, ay, az; // akselerometerverdier (g)
+  float gx, gy, gz; // gyroskopverdier (grader per sekund)
+
+  // Les akselerometerdata
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(ax, ay, az);
+  }
+
+  // Les gyroskopdata
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(gx, gy, gz);
+  }
+
+  // Skriv ut data
+  Serial.print("Aks: ");
+  Serial.print(ax); Serial.print(", ");
+  Serial.print(ay); Serial.print(", ");
+  Serial.print(az); Serial.print(" g");
+
+  Serial.print(" | Gyro: ");
+  Serial.print(gx); Serial.print(", ");
+  Serial.print(gy); Serial.print(", ");
+  Serial.print(gz); Serial.println(" °/s");
+
+  delay(200);
+}
